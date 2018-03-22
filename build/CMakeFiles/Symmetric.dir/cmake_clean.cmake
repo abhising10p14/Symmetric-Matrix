78adfd3946bcc56abdce6a74dabac7c7369df88e
abhising10p14 <abhisingh10p14@gmail.com>
2018-03-22 12:23:17 +0530

    Updated the build library

---
+++ build/CMakeFiles/Symmetric.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Symmetric.dir/src/Symmetric.cpp.o"
+  "libSymmetric.pdb"
+  "libSymmetric.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Symmetric.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
